3ea0cdbf22b7e30839b5f045bb1567eedc833567
Marcos NS <53747245+MarcosJNS@users.noreply.github.com>
2019-10-21 01:37:33 +0200

    Update main.py

--- DNN-project/DNN/main.py
+++ DNN-project/DNN/main.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 10 13:26:57 2019
-
-@author: marcos
-"""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -26,30 +20,29 @@ import sys
 sys.path.append("C:\Program Files (x86)\IronPython 2.7\Lib")
 
 
-#actions we are going to train and predict
+#Actions we are going to train and predict.
 ACTIONS_pan = ['NaN', 'Place', 'Remove']
 actions=ACTIONS_pan
 
-#fram span to infer actions 
+#Frame span to infer actions. 
 N=30   
 
 #When analyzing a video the frames it advances in each iteration. An iteration is trying to get an action in 30 frames, 
 #the first one will be from 0 to 30, then from 2 to 32 and so forth.
-
 Steps=2
 
 #Normalization of the tracking from 'classified' folder for training and val.
 DNNAnalysis_Lib.train_test_teasers_split('pan',N,actions)
 
-#Normalization of prediction info for prediction or for training with multiple actions (read info in folders)
-#DNNAnalysis_Lib.pred_norm('pan',N,Steps)
+#Normalization of prediction info for prediction or for training with multiple actions (read info in folders).
+#DNNAnalysis_Lib.pred_norm('pan',N,Steps).
 
-#Split of prediction data (already cathegorized)
-#DNNAnalysis_Lib.just_split('pan',N,actions)
+#Split of prediction data (already cathegorized).
+#DNNAnalysis_Lib.just_split('pan',N,actions).
 
-#train if predict=True, instead of using Action_train and val, it will use Predict_train and val.
+#Train, if predict=True, instead of using Action_train and val, it will use Predict_train and val.
 DNNAnalysis_Lib.train('pan',N,actions,train_with_predict=False)
 
 
-#prediction, creates a new csv file with prediction
+#Prediction, creates a new csv file with prediction.
 DNNAnalysis_Lib.predict('pan',N,actions)
